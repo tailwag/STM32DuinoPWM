af672985ffe8ee7793e51887aaa6a157bc75b36a
Devin Shoemaker <devin@shoemaker.info>
2025-11-12 19:31:54 -0500

    formatting

--- STM32DuinoPWM.hpp
+++ STM32DuinoPWM.hpp
@@ -3,8 +3,8 @@
 #include <cstdint>
 
 enum PwmRange {
-  LOWFREQ, 
-  HIGHFREQ,
+    LOWFREQ, 
+    HIGHFREQ,
 };
 
 TIM_TypeDef *getTimerForPin(uint8_t pin);
@@ -15,74 +15,73 @@ int8_t getOutputIdForPin(uint8_t pin);
  *  -- BASE TIMER CLASS      --  *
  *  ---------------------------  */
 class STM32HALPWM {
-  protected:
-    HardwareTimer *halTimer;
-    uint8_t pwmPin;
-    int8_t channel;
-    int8_t outputId; 
-    bool initialized;
+    protected:
+        HardwareTimer *halTimer;
+        uint8_t pwmPin;
+        int8_t channel;
+        int8_t outputId; 
+        bool initialized;
 
-  public:
-    STM32HALPWM(uint8_t pin);
-    virtual ~STM32HALPWM();
+    public:
+        STM32HALPWM(uint8_t pin);
+        virtual ~STM32HALPWM();
 
-    virtual void begin() = 0;
-    virtual void   end() = 0;
+        virtual void begin() = 0;
+        virtual void   end() = 0;
 
-    TIM_TypeDef *getTimerInstance() const;
-    uint8_t getChannel() const { return channel; }
-
+        TIM_TypeDef *getTimerInstance() const;
+        uint8_t getChannel() const { return channel; }
 };
 
 /*  ---------------------------  *
  *  -- PWM GENERATOR CLASS   --  *
  *  ---------------------------  */
 class OutputPWM : public STM32HALPWM {
-  private:
-    float frequency;
-    float dutyCycle;
-    bool  state = false;
+    private:
+        float frequency;
+        float dutyCycle;
+        bool  state = false;
 
-  public:
-    OutputPWM(uint8_t pin, float freq = 128, float duty = 50.0f);
+    public:
+        OutputPWM(uint8_t pin, float freq = 128, float duty = 50.0f);
 
-    void begin() override;
-    void   end() override;
+        void begin() override;
+        void   end() override;
 
-    void enable();
-    void disable();
+        void enable();
+        void disable();
 
-    void setFrequency(float freq);
-    void setDutyCycle(float duty);
+        void setFrequency(float freq);
+        void setDutyCycle(float duty);
 
-    float getFrequency() const { return frequency; }
-    float getDutyCycle() const { return dutyCycle; }
-    bool  getState()     const { return state; }
+        float getFrequency() const { return frequency; }
+        float getDutyCycle() const { return dutyCycle; }
+        bool  getState()     const { return state; }
 };
 
 /*  ---------------------------  *
  *  -- PWM MEASUREMENT CLASS --  *
  *  ---------------------------  */
 class InputPWM : public STM32HALPWM {
-  private:
-    volatile uint32_t lastRising;
-    volatile uint32_t lastFalling;
-    volatile uint32_t period;
-    volatile uint32_t pulseWidth;
-    volatile uint32_t lastCapture;
+    private:
+        volatile uint32_t lastRising;
+        volatile uint32_t lastFalling;
+        volatile uint32_t period;
+        volatile uint32_t pulseWidth;
+        volatile uint32_t lastCapture;
 
-    bool signalPresent = false; 
+        bool signalPresent = false; 
 
-    uint16_t prescalerValue; 
-    static void captureCallback(void *arg);
+        uint16_t prescalerValue; 
+        static void captureCallback(void *arg);
 
-  public:
-    InputPWM(uint8_t pin, PwmRange range=LOWFREQ);
-    void handleCapture(); 
+    public:
+        InputPWM(uint8_t pin, PwmRange range=LOWFREQ);
+        void handleCapture(); 
 
-    void begin() override;
-    void   end() override;
+        void begin() override;
+        void   end() override;
 
-    float getFrequency();
-    float getDutyCycle();
+        float getFrequency();
+        float getDutyCycle();
 };
